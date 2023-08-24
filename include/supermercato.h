7ae2768fdcd7943c5e5d82925bd571a95fa46a0d
luigiarena <luigiarena.cs@gmail.com>
2023-08-24 17:56:01 +0200

    Aggiunta della struttura parametri in supermercato.h

--- include/supermercato.h
+++ include/supermercato.h
@@ -1,1 +1,11 @@
- 
+#define BUF_SIZE 128
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+
+typedef struct param {
+    int K, C, E, T, P, S1, S2, TP, Q, R;
+} param;
