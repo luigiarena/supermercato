822a4e87e334f67b2e20d485925c1d8bb9f85784
luigiarena <luigiarena.cs@gmail.com>
2023-08-24 13:11:21 +0200

    Aggiunta gestione degli argomenti e apertura file di configurazione

--- src/supermercato.c
+++ src/supermercato.c
@@ -6,9 +6,39 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
 
 //  Funzione main del progetto
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     FILE *config;
+    char* config_name;
+
+    // Controllo gli argomenti
+    switch (argc) {
+        case 1:{
+            config_name = "../config.txt";
+            break;
+        }
+        case 3:{
+            if (strcmp(argv[1], "-c")==0 || strcmp(argv[1], "-C")==0) {
+                config_name = argv[2];
+                break;
+            }
+        }
+        default: {
+            fprintf(stderr, "Usage: %s [-c] [nomeFile]\n", argv[0]);
+            exit(errno);
+            break;
+        }
+    }
+
+    // Provo ad aprile il file di configurazione
+    if((config = fopen(config_name, "r")) == NULL) {
+        perror("Opening configuration file");
+        exit(errno);
+    }
+    
 
 }
