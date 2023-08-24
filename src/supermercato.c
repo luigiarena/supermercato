a94c86f193504713852788107838653351f6ce0c
luigiarena <luigiarena.cs@gmail.com>
2023-08-24 12:02:57 +0200

    Creato file supermercato.c

---
+++ src/supermercato.c
@@ -0,0 +1,14 @@
+/*
+    File: supermercato.c
+    Specifica: file principale del progetto, implementa tutte le procedure di avvio e chiusura
+    Autore: Arena Luigi
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//  Funzione main del progetto
+int main(int argc, char *argv[]) {
+    FILE *config;
+
+}
