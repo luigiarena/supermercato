292a70a791c6f8aa07aa791e8c70751a810861eb
luigiarena <luigiarena.cs@gmail.com>
2023-08-26 19:24:03 +0200

    Aggiunta del ciclo per leggere i parametri da config.txt e della funzione per eliminare gli spazi vuoti nelle righe lette

--- src/supermercato.c
+++ src/supermercato.c
@@ -6,6 +6,22 @@
 
 #include "supermercato.h"
 
+//param* config = malloc(sizeof(param));
+
+void remove_spaces(char buffer[]) {
+    int i=0, j=1;
+    while (buffer[i] != '\0' && i < BUF_SIZE-1 && j < BUF_SIZE-1) {
+        //printf("-iter i: %d\n", i);
+        if (buffer[i] == ' ') {
+            while (buffer[j] == ' ' && buffer[j] != '\0' && j < BUF_SIZE) j++; 
+            buffer[i] = buffer[j];
+            buffer[j] = ' ';         
+        }
+        if (buffer[i] == '\0') return;
+        i++; j++;
+    }
+}
+
 //  Funzione main del progetto
 int main(int argc, char* argv[]) {
     int opt, cflag = 0, vflag = 0;
@@ -20,7 +36,7 @@ int main(int argc, char* argv[]) {
             case 'v':
                 vflag = 1; break;
             case 'h':
-                fprintf(stderr, "Usage: %s [-c] [nomeFile]\n", argv[0]);
+                fprintf(stderr, "Usage: %s [-h] [-v] [-c] [nomeFile]\n", argv[0]);
                 exit(errno); break;
             default:
                 fprintf(stderr, "No valid argument\n");
@@ -39,5 +55,19 @@ if(vflag==1) printf("nome file: %s\ncflag: %d\nvflag: %d\n", config_name, cflag,
         exit(errno);
     }
 
+    // Leggo i valori dei parametri e li salvo nella struttura param
+    char buffer[BUF_SIZE];
+    char* token;
+
+    while (fgets(buffer, BUF_SIZE, config_file) != NULL) {
+        remove_spaces(buffer);
+        if (buffer[0] == '#' || buffer[0] == '\n') continue;
+        printf("%s", buffer);
+        token = strtok(buffer, ":");
+        /*while (token) {
+            //switch()
+        }
+        */
+    }
 
 }
