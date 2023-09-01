aa7492022dea4f1a1a94fa71205ec62ea65da2fa
luigiarena <luigiarena.cs@gmail.com>
2023-09-01 20:23:58 +0200

    Modifica delle macro IFERROR per generalizzare il valore di confronto

--- include/supermercato.h
+++ include/supermercato.h
@@ -13,8 +13,10 @@
 #define STDOUT 1
 #define STDERR 2
 
-#define IFERROR(s,m) if((s)==-1) {perror(m); exit(errno);}
-#define IFERROR3(s,m,c) if((s)==-1) {perror(m); c;}
+#define IFERROR(s,v,m) if((s)==v) {perror(m); exit(errno);}
+#define IFERROR3(s,v,m,c) if((s)==v) {perror(m); c;}
+#define IFERRORM1(s,m) if((s)==-1) {perror(m); exit(errno);}
+#define IFERROR3C(s,m,c) if((s)==-1) {perror(m); c;}
 
 #define WRITE(m) IFERROR(write(STDOUT,m,strlen(m)), m);
 #define WRITELN(m) WRITE(m);WRITE("\n");
