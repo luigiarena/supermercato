3b89b486616b3632dfe13a184f15a3f40e7ee50e
luigiarena <luigiarena.cs@gmail.com>
2023-09-04 22:16:50 +0200

    Aggiunta delle strutture base di Clienti e Cassieri. Aggiunto primo include in direttore.h

--- src/cassiere.c
+++ src/cassiere.c
@@ -1,1 +1,5 @@
- 
+typedef struct Cassiere {
+        long id;                // identificatore numerico della cassa
+        int prodotti;           // numero di prodotti elaborati
+        int clienti;            // numero di clienti passati
+} Cassiere;
--- src/cliente.c
+++ src/cliente.c
@@ -1,1 +1,4 @@
- 
+typedef struct Cliente {
+        long id;                // identificatore numerico del cliente
+        int nprodotti;          // numero di prodotti acquistati
+} Cliente;
