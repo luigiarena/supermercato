ce27a20290a1665aead133a3ba6d585bfca8846d
luigiarena <luigiarena.cs@gmail.com>
2023-09-20 16:19:53 +0200

    Definizione della struttura param config da supermercato.h a utilis.h per includerla anche in direttore.h

--- include/utils.h
+++ include/utils.h
@@ -1,3 +1,6 @@
+#ifndef utils_h
+#define utils_h
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -24,3 +27,20 @@
 
 #define WRITE(m) IFERROR(write(STDOUT,m,strlen(m)), m);
 #define WRITELN(m) WRITE(m);WRITE("\n");
+
+/*  Struttura che contiene i parametri di configurazione    */
+typedef struct param {
+    int K;  // Numero max di casse aperte
+    int Ki; // Numero di casse aperte inizialmente
+    int C;  // Numero max di clienti
+    int E;  // C-E è la soglia per far entrare nuovi clienti
+    int P;  // Numero max di prodotti acquistabili
+    int T;  // Tempo di acquisto per cliente
+    int Tp; // Tempo di controllo per ogni prodotto alla cassa
+    int Td; // (ms) intervallo aggiornamento del direttore dai cassieri
+    int S;  // (ms) intervallo controllo coda per cliente
+    int S1; // Soglia per la chiusura delle casse
+    int S2; // Soglia per l'apertura delle casse
+} param;
+
+#endif
